cd33d9b5d69bb8f20a603a196abc5b951fe82de1
Fearium <jessebaril.programmer@gmail.com>
2016-04-13 18:07:29 -0400

    added rough a teams and tournament creation front-end

---
+++ server/models/teams.ts
@@ -0,0 +1,61 @@
+"use strict";
+var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+// DEFINE THE OBJECT SCHEMA
+var teamsSchema = new mongoose.Schema({
+    teamname: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'teamname is required'
+    },
+    createdby: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'createdby is required'
+    },
+    player1: {
+        type: String,
+        default: '',
+        trim: true,
+    },
+    player2: {
+        type: String,
+        default: '',
+        trim: true,
+    },
+    player3: {
+        type: String,
+        default: '',
+        trim: true,
+    },
+    player4: {
+        type: String,
+        default: '',
+        trim: true,
+    },
+    player5: {
+        type: String,
+        default: '',
+        trim: true,
+    },
+    player6: {
+        type: String,
+        default: '',
+        trim: true,
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date,
+        default: Date.now
+    }
+}, { collection: 'teamInfo' });
+teamsSchema.plugin(passportLocalMongoose);
+// MAKE THIS PUBLIC SO THE CONTROLLER CAN SEE IT
+exports.Teams = mongoose.model('Teams', teamsSchema);
+
+//# sourceMappingURL=teams.js.map
